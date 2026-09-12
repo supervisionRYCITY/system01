9ef6294dca46313679cabf2e071d7e628e7c80cd
supervisionRYCITY <supervision@timtaram.ac.th>
2026-09-12 18:48:56 +0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -68,8 +68,8 @@ function loadNews() {
 
       
       row.innerHTML = data.map(n => `
-        <article class="scroll-item shrink-0 w-[161px] doc-card rounded-xl overflow-hidden">
-          <div class="relative h-[92px] bg-line">
+        <article class="scroll-item shrink-0 w-[242px] doc-card rounded-xl overflow-hidden">
+        <div class="relative h-[92px] bg-line">
             <img src="${n.Cover_Image_URL}" alt="" class="w-full h-full object-cover" onerror="this.style.display='none'">
             ${isTrue(n.Is_Pinned) ? '<span class="absolute top-1.5 right-1.5 bg-gold text-white text-xs px-2 py-0.5 rounded-full"><i class="fa-solid fa-thumbtack"></i></span>' : ''}
           </div>
