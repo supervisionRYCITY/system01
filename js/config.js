f0d098d5ed349a92712e6c020460a715d723de6a
supervisionRYCITY <supervision@timtaram.ac.th>
2026-09-12 12:19:31 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,4 +3,4 @@
 // วิธีหา URL: ใน GAS ไปที่ Deploy > Manage deployments > คัดลอก Web app URL
 // (ต้อง Deploy แบบ Execute as: Me / Who has access: Anyone)
 // ---------------------------------------------------------------
-const API_BASE_URL = 'https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXX/exec'; // TODO: ใส่ URL จริงหลัง Deploy Code.gs
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbwYUIThqcQ9relw12mbkhGgtJ-rY__Bi5K3M_HGxuTDJxrrHLp7LojI-e5H3l_R4tiQbw/exec'; // TODO: ใส่ URL จริงหลัง Deploy Code.gs
